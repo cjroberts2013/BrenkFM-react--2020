c654a815f1bfa0c87359a5d49812b9a953d05f90
CJ Roberts <cjroberts2013@gmail.com>
2020-07-31 21:25:48 -0500

    Full react environment set up using babel and webpack

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./styles/styles.scss";
+
+const template = <h1>Toasty</h1>
+
+
+ReactDOM.render(template, document.getElementById('app'))
